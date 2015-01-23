d392b3fa278cc97c56b072937a93d9842155a4a9
magda <magdasanchez@colaborativa.eu>
2015-01-23 20:49:44 +0100

    adding compartio schemas

---
+++ schema/City.js
@@ -0,0 +1,23 @@
+// app/model/city.js
+// load the cities we need
+//
+
+var mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+
+// define the schema for our city model
+var citySchema = new Schema({
+  name          : { type: String, required: true},  
+  postal_code   : { type: Number},
+  slug          : { type: String},
+  isActive      : { type: Boolean, default: false}, 
+  created       : { type: Date, default: Date.now },
+  updated       : { type: Date },
+  Country       : {
+                    type: Schema.ObjectId,
+                    ref: 'Country'
+                 }
+});
+
+// create the model "City" for cities and expose it to our app
+module.exports = mongoose.model('City', citySchema);
