cf3f78c1f7e8849ceed30602f69fddcae994d8c3
magda <magdasanchez@colaborativa.eu>
2015-01-26 19:27:22 +0100

    Fixing out problems with schemas definition

--- models.js
+++ models.js
@@ -1,17 +1,29 @@
 'use strict';
-/* All the schemas are included here */
+/* All the schemas are included here  15 */
 
 exports = module.exports = function(app, mongoose) {
-  //embeddable docs first
-  require('./schema/Note')(app, mongoose);
+  // Embeddable docs first DRYWALL 
   require('./schema/Status')(app, mongoose);
-  require('./schema/StatusLog')(app, mongoose);
   require('./schema/Category')(app, mongoose);
 
-  //then regular docs
+  // Then regular docs DRYWALL
   require('./schema/User')(app, mongoose);
-  require('./schema/Admin')(app, mongoose);
   require('./schema/AdminGroup')(app, mongoose);
+  require('./schema/Admin')(app, mongoose);
   require('./schema/Account')(app, mongoose);
   require('./schema/LoginAttempt')(app, mongoose);
+
+  require('./schema/Note')(app, mongoose); 
+  require('./schema/StatusLog')(app, mongoose); //
+
+  // Compartio schemas 6
+  require('./schema/Country')(app, mongoose);
+  require('./schema/City')(app, mongoose);
+
+  require('./schema/Compartiocategory')(app, mongoose);
+
+  require('./schema/Feedback')(app, mongoose);
+
+  require('./schema/Compartio')(app, mongoose);
+  require('./schema/Message')(app, mongoose);
 };
--- schema/City.js
+++ schema/City.js
@@ -4,17 +4,14 @@
 //
 exports = module.exports = function(app, mongoose) {
 // define the schema for our city model
-  var citySchema = new Schema({
+  var citySchema = new mongoose.Schema({
     name          : { type: String, required: true},  
     postal_code   : { type: Number},
     slug          : { type: String},
     isActive      : { type: Boolean, default: false}, 
     created       : { type: Date, default: Date.now },
     updated       : { type: Date },
-    Country       : {
-                      type: Schema.ObjectId,
-                      ref: 'Country'
-    }
+    Country       : { type: mongoose.Schema.Types.ObjectId, ref: 'Country'}
   });
   app.db.model('City', citySchema);
 };
