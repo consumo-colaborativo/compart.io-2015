aa3f993e929ee8549a18cb20404ab576df219b29
Álvaro Saco <alvarosaco@gmail.com>
2015-05-20 19:19:44 -0500

    Añadiendo el campo "about"

--- schema/User.js
+++ schema/User.js
@@ -8,6 +8,7 @@ exports = module.exports = function(app, mongoose) {
     username: { type: String, unique: true },
     password: String,
     email: { type: String, unique: true },
+    about: String,
     roles: {
       admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin' },
       account: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' }
